3300ccca21253968d7c719de5622ce94be6c1d1d
Roberto Luna Victoria Monzon <roberto971968@hotmail.com>
2020-10-02 12:45:55 -0500

    comienzo el curso, voy a poner anotaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+//Modulos
+import { PagesModule } from './pages/pages.module';
+import { IncrementadorComponent } from './components/incrementador/incrementador.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    IncrementadorComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     AppRoutingModule
   ],
   providers: [],
